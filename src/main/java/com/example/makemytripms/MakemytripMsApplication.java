452aa371dc767d173e679a46080889289ed3095b
nishi <nishi@gmail.com>
2022-12-28 22:08:41 +0530

    hello

---
+++ src/main/java/com/example/makemytripms/MakemytripMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.makemytripms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemytripMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemytripMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/makemytripms/MakemytripMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makemytripms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemytripMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
